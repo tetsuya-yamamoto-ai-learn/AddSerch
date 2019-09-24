21b4587b7320fe2481d60b27c337355e153ab7d5
tetsuya-yamamoto-ai-learn <tetsuya.yamamoto.ai.study@gmail.ocm>
2019-09-24 11:30:36 +0900

    zipcloudのWebAPIを利用して郵便番号から住所を取得する機能を実装
    - requests.get()の練習
    - WebAPIを利用したJSONのパース
    - 変数の抽出・変数のインライン化

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import requests
+
+
 def main():
-    pass
+    # 1600023 -> '東京都新宿区西新宿'
+    # 入力
+    zipcode = str(input("input zipcode ? > "))
+    url = f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}'
+
+    # 計算
+    # リクエスト送る　→　受け取ってパース(構造解析＋抽出)　(→　フォーマット)
+    response = requests.get(url)  # import requests必要
+
+    # 出力
+    if response.status_code == 200:
+        address = response.json()["results"][0]
+
+        都道府県 = address["address1"]  # 東京都
+        市区町村 = address["address2"]  # 新宿区
+        町域 = address["address3"]  # 西新宿
+        print(f'{都道府県} {市区町村} {町域}')
 
 
 if __name__ == '__main__':
