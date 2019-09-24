02d9b5fbe223538bafbe7747fbe874302383f36b
tetsuya-yamamoto-ai-learn <tetsuya.yamamoto.ai.study@gmail.ocm>
2019-09-24 11:39:11 +0900

    郵便番号を入力する機能を追加
    - inputで郵便番号を受け取る
    - 不正な入力があった場合メッセージを出力

--- main.py
+++ main.py
@@ -8,17 +8,19 @@ def main():
     url = f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}'
 
     # 計算
-    # リクエスト送る　→　受け取ってパース(構造解析＋抽出)　(→　フォーマット)
     response = requests.get(url)  # import requests必要
 
     # 出力
-    if response.status_code == 200:
+    if response.json()['status'] == 200:
         address = response.json()["results"][0]
 
         都道府県 = address["address1"]  # 東京都
         市区町村 = address["address2"]  # 新宿区
         町域 = address["address3"]  # 西新宿
         print(f'{都道府県} {市区町村} {町域}')
+
+    else:
+        print(response.json()['message'])
 
 
 if __name__ == '__main__':
